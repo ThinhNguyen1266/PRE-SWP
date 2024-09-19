a91c59f4cdafa98b5e10c4ebe6532a2bf196520b
Nguyen Cuong Thinh <ThinhNgyenCTH@gmail.com>
2024-09-19 13:18:24 +0700

    init project

---
+++ back-end/src/test/java/swp/pre/swp/SwpApplicationTests.java
@@ -0,0 +1,13 @@
+package swp.pre.swp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
