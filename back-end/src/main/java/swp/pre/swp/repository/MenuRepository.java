826909fbf64a068f1446831ff46e060939b247df
Lily Huynh <lily.huynh@squareonesoft.com>
2024-09-22 17:53:02 +0700

    menu manage

--- back-end/src/main/java/swp/pre/swp/repository/MenuRepository.java
+++
@@ -1,8 +0,0 @@
-package swp.pre.swp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import swp.pre.swp.entity.MenuEntity;
-
-@Repository
-public interface MenuRepository extends JpaRepository<MenuEntity,Integer> {
-}
